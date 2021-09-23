d351955a632ea46ff37400ce9e4b543367de6604
mykywar <mr.kywar@gmail.com>
2021-09-23 22:32:23 +0200

    [WIP] Hack Collection Adapter

--- modules/php/Adapters/CollectionAdapter.php
+++ modules/php/Adapters/CollectionAdapter.php
@@ -13,7 +13,7 @@ abstract class CollectionAdapter {
 
     public static function adapt($results) {
         $collections = [];
-        if (null === $results || empty($results)) {
+        if (1===1 || null === $results || empty($results)) { // tempory hack !
             return $collections;
         } else {
             foreach ($results as $card) {      
