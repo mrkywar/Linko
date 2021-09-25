068fd21e96c3d124a18ad5e37d29859f60f182b8
mykywar <mr.kywar@gmail.com>
2021-09-25 23:52:51 +0200

    [WIP] Send Notification

--- modules/php/States/Traits/PlayCardTrait.php
+++ modules/php/States/Traits/PlayCardTrait.php
@@ -6,6 +6,7 @@ use Linko\Managers\Deck\Deck;
 use Linko\Managers\GlobalVarManager;
 use Linko\Managers\Logger;
 use Linko\Managers\PlayerManager;
+use Linko\Models\Card;
 use Linko\Models\GlobalVar;
 
 /**
@@ -18,15 +19,16 @@ trait PlayCardTrait {
      *            BEGIN - Play Cards Actions
      * ---------------------------------------------------------------------- */
 
-    public function actionPlayCards($cardIds) {
-        Logger::log("Action Play Card " . $cardIds, "PCT-APC");
+    public function actionPlayCards($rawCardIds) {
+        Logger::log("Action Play Card " . $rawCardIds, "PCT-APC");
+        $cardIds = explode(",", $rawCardIds);
 
         $cardManager = $this->getCardManager();
         $cardRepo = $cardManager->getRepository();
         $playerId = self::getActivePlayerId();
         $cards = $cardRepo
                 ->setDoUnserialization(true)
-                ->getById(explode(",", $cardIds));
+                ->getById($cardIds);
 
         $checkPosition = $this->checkPosition($cards, $playerId);
         $checkNumber = $this->checkNumbers($cards);
@@ -38,52 +40,75 @@ trait PlayCardTrait {
         $collectionIndex = $cardRepo->getNextCollectionIndex($playerId);
         $cardRepo->moveCardsToLocation($cards, $destination, $collectionIndex);
 
-        $this->afterActionPlayCards();
+        $this->afterActionPlayCards($cardIds, $cards);
     }
 
     /* -------------------------------------------------------------------------
      *            BEGIN - Play Cards Actions - TOOLS
      * ---------------------------------------------------------------------- */
 
-    private function checkPosition(array $cards, $playerId) {
+    private function checkPosition($cards, $playerId) {
         $checkPosition = true;
-        foreach ($cards as $card) {
+        if (is_array($cards)) {
+            foreach ($cards as $card) {
+                $checkPosition = $checkPosition &&
+                        Deck::HAND_NAME === $card->getLocation() &&
+                        $playerId === $card->getLocationArg();
+            }
+        } else {
             $checkPosition = $checkPosition &&
-                    Deck::HAND_NAME === $card->getLocation() &&
-                    $playerId === $card->getLocationArg();
+                    Deck::HAND_NAME === $cards->getLocation() &&
+                    $playerId === $cards->getLocationArg();
         }
         return $checkPosition;
     }
-    
-    private function checkNumbers(array $cards) {
+
+    private function checkNumbers($cards) {
         $numbers = null;
         $countNumber = 0;
         $countJoker = 0;
         $checkNumber = true;
-        
+        if ($cards instanceof Card) {
+            return $checkNumber; // no collection only one card
+        }
         foreach ($cards as $card) {
-            if (14 === $card->getType()){
-                $countJoker ++;
-            }elseif(null === $numbers){
+            if (14 === $card->getType()) {
+                $countJoker++;
+            } elseif (null === $numbers) {
                 $numbers = $card->getType();
                 $countNumber++;
-            }elseif($numbers === $card->getType()){
+            } elseif ($numbers === $card->getType()) {
                 $countNumber++;
-            }else{
+            } else {
                 $checkNumber = false;
             }
         }
-        
+
         return $checkNumber && (($countNumber + $countJoker) === count($cards));
-        
-        
     }
 
-    private function afterActionPlayCards() {
+    private function afterActionPlayCards(array $cardsId, $cards) {
         $stateManager = $this->getStateManager();
         $newState = $stateManager->closeActualState();
 
         Logger::log("NextState : " . $newState->getState());
+
+        if (1 === sizeof($cardsId)) {
+            $number = $cards->getType();
+        }else{
+            $number = $cards[0]->getType();
+        }
+
+        self::notifyAllPlayers("playNumber", clienttranslate('${playerName} plays a collection of ${count} card(s) with a value of ${number}'),
+                [
+                    'playerId' => self::getActivePlayerId(),
+                    'playerName' => self::getActivePlayerName(),
+                    'count' => count($cardsId),
+                    'number' => $number,
+                    'ids' => $cardsId
+                ]
+        );
+
         $this->gamestate->jumpToState($newState->getState());
     }
 
