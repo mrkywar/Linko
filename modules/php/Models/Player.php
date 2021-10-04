caa14b8df18e85df88351cbaceda0c363676fc87
mykywar <mr.kywar@gmail.com>
2021-10-04 21:45:46 +0200

    [UNSTABLE] Player Model

--- modules/php/Models/Player.php
+++ modules/php/Models/Player.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Linko\Models;
 
 /**
@@ -6,6 +7,181 @@ namespace Linko\Models;
  *
  * @author Mr_Kywar mr_kywar@gmail.com
  */
-class Player {
-    //put your code here
+class Player extends \stdClass {
+
+    /**
+     * 
+     * @var int
+     * @ORM\Column(type="integer", name="player_id")
+     */
+    private $id;
+
+    /**
+     * 
+     * @var int
+     * @ORM\Column(type="integer", name="player_no")
+     */
+    private $no;
+
+    /**
+     * 
+     * @var string
+     * @ORM\Column(type="string", name="player_name")
+     */
+    private $name;
+
+    /**
+     * 
+     * @var string
+     * @ORM\Column(type="string", name="player_color")
+     */
+    private $color;
+
+    /**
+     * 
+     * @var string
+     * @ORM\Column(type="string", name="player_canal")
+     */
+    private $canal;
+
+    /**
+     * 
+     * @var string
+     * @ORM\Column(type="string", name="player_avatar")
+     */
+    private $avatar;
+
+    /**
+     * 
+     * @var bool
+     * @ORM\Column(type="boolean", name="player_ai")
+     */
+    private $isAi;
+
+    /**
+     * 
+     * @var int
+     * @ORM\Column(type="int", name="player_score")
+     */
+    private $score;
+
+    /**
+     * 
+     * @var bool
+     * @ORM\Column(type="boolean", name="player_zombie")
+     */
+    private $isZombie;
+
+    /**
+     * 
+     * @var bool
+     * @ORM\Column(type="boolean", name="player_zombie")
+     */
+    private $isEliminated;
+
+    /* -------------------------------------------------------------------------
+     *                  BEGIN - Constructor
+     * ---------------------------------------------------------------------- */
+
+    public function __construct() {
+        $this->isAi = false;
+        $this->isEliminated = false;
+        $this->isZombie = false;
+        $this->score = 0;
+    }
+
+    /* -------------------------------------------------------------------------
+     *                  BEGIN - Getters & Setters 
+     * ---------------------------------------------------------------------- */
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getNo(): int {
+        return $this->no;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getColor(): string {
+        return $this->color;
+    }
+
+    public function getCanal(): string {
+        return $this->canal;
+    }
+
+    public function getAvatar(): string {
+        return $this->avatar;
+    }
+
+    public function getIsAi(): bool {
+        return $this->isAi;
+    }
+
+    public function getScore(): int {
+        return $this->score;
+    }
+
+    public function getIsZombie(): bool {
+        return $this->isZombie;
+    }
+
+    public function getIsEliminated(): bool {
+        return $this->isEliminated;
+    }
+
+    public function setId(int $id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setNo(int $no) {
+        $this->no = $no;
+        return $this;
+    }
+
+    public function setName(string $name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setColor(string $color) {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function setCanal(string $canal) {
+        $this->canal = $canal;
+        return $this;
+    }
+
+    public function setAvatar(string $avatar) {
+        $this->avatar = $avatar;
+        return $this;
+    }
+
+    public function setIsAi(bool $isAi) {
+        $this->isAi = $isAi;
+        return $this;
+    }
+
+    public function setScore(int $score) {
+        $this->score = $score;
+        return $this;
+    }
+
+    public function setIsZombie(bool $isZombie) {
+        $this->isZombie = $isZombie;
+        return $this;
+    }
+
+    public function setIsEliminated(bool $isEliminated) {
+        $this->isEliminated = $isEliminated;
+        return $this;
+    }
+
 }
